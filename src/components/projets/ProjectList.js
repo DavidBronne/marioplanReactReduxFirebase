403b9083f8e730106b416a719f980c6fc0df47ac
David Bronne <davidbronne@gmail.com>
2021-05-25 11:57:13 +0200

    component: Project summary

--- src/components/projets/ProjectList.js
+++ src/components/projets/ProjectList.js
@@ -1,32 +1,13 @@
 import React from 'react';
+import ProjectSummary from './ProjectSummary'
 
 const ProjectList = () => {
     return (
         <div className="project-list section">
-
-            <div className="card">
-                <div className="card-content">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by Eliot</p>
-                    <p className="grey-text">May 25th 11:25</p>
-                </div>
-            </div>
-
-            <div className="card">
-                <div className="card-content">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by Eliot</p>
-                    <p className="grey-text">May 25th 11:25</p>
-                </div>
-            </div>
-
-            <div className="card">
-                <div className="card-content">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by Eliot</p>
-                    <p className="grey-text">May 25th 11:25</p>
-                </div>
-            </div>
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />
        
         </div>
     );
---
+++ src/components/projets/ProjectSummary.js
@@ -0,0 +1,15 @@
+import React from 'react-router-dom';
+
+const ProjectSummary = () => {
+    return (
+        <div className="card">
+                <div className="card-content">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by Eliot</p>
+                    <p className="grey-text">May 25th 11:25</p>
+                </div>
+            </div>
+    );
+}
+ 
+export default ProjectSummary;
